8bf59a229d8cca36a25b10cc4318e858f4c2c02f
Flitz08 <marcosscotaro1315@gmail.com>
2024-12-05 19:21:13 -0300

    MediaQuerys terminada por el momento

--- app.js
+++ app.js
@@ -1,16 +1,75 @@
-let menuOculto = document.querySelector(".menuJS")
+let menuOculto = document.getElementById("menuJS")
+
+menuOculto.addEventListener("click", ocultarCosas)
 
 
-menuOculto.forEach(e=>{
-    e.addEventListener("click",alert("asda"))
-});
+let displayNavbar = document.querySelectorAll(".busqueda")
+let menuPadre =  document.querySelectorAll(".menuPadre")
+let barraNavegacion = document.querySelector(".barraNavegacion")
+let menuOpen = false
+let icono = document.querySelectorAll(".menuContenedor")
+let user = document.getElementById("user")
 
-addEventListener
-
-
-
-
-
-//corregir lo de arriba
+let xOculta = document.getElementById("xJS")
 
 
+xOculta.addEventListener("click", ocultarCosas)
+
+
+
+function ocultarCosas(){
+    let openClose = window.getComputedStyle(barraNavegacion)
+    if (openClose.height === "100px"){
+        /*Aca se maneja el evento cuando la pantalla a 400px */
+        barraNavegacion.style.height = "400px"
+        xOculta.style.display = "flex"
+        menuOculto.style.display ="none"
+        setTimeout(()=>{
+            icono.forEach(function(display){
+                display.style.display = "flex"
+            })
+            user.style.display = "flex"
+        },400)
+    } else if ( openClose.height === "400px"){
+        /*Aca se maneja el evento cuando se cierra la panalla */
+        barraNavegacion.style.height = "100px"
+        menuOculto.style.display = "flex"
+        xOculta.style.display = "none"
+        icono.forEach(function(display){
+            display.style.display = "none"
+        })
+        user.style.display = "none"
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function alerta (){
+    alert("asd")
+}
--- readme.txt
+++ readme.txt
@@ -24,3 +24,10 @@ puma verde : https://www.artstation.com/artwork/9m5z0a artista david abasto
 
 
 
+
+
+
+Cosas para mejorar., 
+
+
+en el navbar, cuando esta de 768 px abajo aparece la lupa para buscar pero no sale un input. Ver como colocar este imput
